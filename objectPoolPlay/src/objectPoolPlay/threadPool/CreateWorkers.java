ccec7e8ab89aa1bcf4dffa559d4b18ab4be97ce8
sagar <sagar@192.168.0.5>
2020-03-30 20:07:56 -0400

    creatworker class updated

---
+++ objectPoolPlay/src/objectPoolPlay/threadPool/CreateWorkers.java
@@ -0,0 +1,37 @@
+package objectPoolPlay.threadPool;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import objectPoolPlay.util.FileProcessor;
+import objectPoolPlay.util.IsPrime;
+import objectPoolPlay.util.ResultI;
+
+public class CreateWorkers {
+
+	private FileProcessor fp;
+	private ResultI result;
+	private IsPrime isPrime;
+
+	public CreateWorkers(FileProcessor fpIn, ResultI resultIn, IsPrime isPrimeIn) {
+
+		fp = fpIn;
+		result = resultIn;
+		isPrime = isPrimeIn;
+	}
+	
+	public void startWorkers(int numOfThreads) throws InterruptedException {
+
+		ThreadPool pool = new ThreadPool();
+		pool.newFixedPool(numOfThreads, fp, result, isPrime);
+		
+		while(!pool.getThreadList().isEmpty()) {
+			Thread t = new Thread(pool.borrow());
+			t.start();
+			t.join();			
+		}
+		
+		
+	}
+
+}
